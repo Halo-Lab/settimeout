3a15ca8f7739ceb53b235b0c2b8a5c246b6fa7a9
Igor Klyopov <shoot8202@gmail.com>
2021-12-14 13:08:05 +0200

    adds draft of subscription functionality

--- src/landing/scenes/Main/SubscriptionBlock/SubscriptionBlock.js
+++ src/landing/scenes/Main/SubscriptionBlock/SubscriptionBlock.js
@@ -1,34 +1,62 @@
-import { html } from "@prostory/edelweiss";
+import { html, data, effect, router, current } from "@prostory/edelweiss";
 
 import "./SubscriptionBlock.css";
+import unisenderAPI from "../../../../services/unisenderAPI";
+
+const subscriptionEmail = data("");
+
+const onSubscriptionFormSubmit = async (e) => {
+	e.preventDefault();
+
+	const email = subscriptionEmail();
+
+	try {
+		const contact = await unisenderAPI.getContact(email);
+
+		if (contact.result.email) return current("/already-subscribed"); //- to go to the corresponding page
+
+		const response = await unisenderAPI.addContact(email);
+
+		if (response.result.person_id) return current("/thanks"); //- to go to the corresponding page
+	} catch (error) {
+		//for test
+		console.log(error);
+		if (error) current("/something-wrong");
+		////////////
+	}
+};
 
 export const SubscriptionBlock = html`
 	<section class="subscription">
-		<div class="subscription-container">
-			<div class="subscription-content-wrap">
-				<div class="subscription-content-img"></div>
-				<div class="subscription-content">
-					<h2 class="subscription-appeal-title">Подпишись на рассылку</h2>
-					<p class="subscription-appeal-description">
-						Узнавай первым новости в разработке и достигай большего ежедневно!
-					</p>
-					<form class="subscription-form">
-						<label class="subscription-form-label">
-							<input
-								type="email"
-								placeholder="Ваш Email"
-								autocomplete="email"
-								required
-								name="email-address"
-								id="email-address"
-								class="subscription-form-input"
-							/>
-						</label>
-						<button type="submit" class="subscription-form-btn-submit">
-							Подписаться
-						</button>
-					</form>
-				</div>
+		<div class="subscription-content-wrap">
+			<div class="subscription-content-img"></div>
+			<div class="subscription-content">
+				<h2 class="subscription-appeal-title">Прокачай свой мозг</h2>
+				<p class="subscription-appeal-description">
+					Узнавай первым новости в разработке и достигай большего ежедневно!
+				</p>
+
+				<form
+					class="subscription-form"
+					@submit=${(e) => onSubscriptionFormSubmit(e)}
+				>
+					<label class="subscription-form-label">
+						<input
+							type="email"
+							.value=${() => subscriptionEmail()}
+							placeholder="Ваш Email"
+							autocomplete="email"
+							required
+							name="email-address"
+							id="email-address"
+							class="subscription-form-input"
+							@input=${(e) => subscriptionEmail(e.target.value)}
+						/>
+					</label>
+					<button type="submit" class="subscription-form-btn-submit">
+						Подписаться
+					</button>
+				</form>
 			</div>
 
 			<div class="subscription-footer">
---
+++ src/landing/scenes/SubscriptionAlready/SubscriptionAlready.js
@@ -0,0 +1,5 @@
+import { html } from "@prostory/edelweiss";
+
+export const SubscriptionAlready = html`
+	<h1>Кажется, вы уже подписаны на рассылку...</h1>
+`;
