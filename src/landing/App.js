2fea7a2b31931a849743c38756daed4dbcd1370e
Igor Klyopov <shoot8202@gmail.com>
2021-12-14 14:24:28 +0200

    adds draft of routing to the App.js

--- src/landing/App.js
+++ src/landing/App.js
@@ -1,13 +1,40 @@
-import { html } from "@prostory/edelweiss";
+import { html, router } from "@prostory/edelweiss";
 
 import { AppBar } from "./components/AppBar/AppBar.js";
 import { ArticlesBlock } from "./scenes/Main/ArticlesBlock/ArticlesBlock.js";
 import { SubscriptionBlock } from "./scenes/Main/SubscriptionBlock/SubscriptionBlock.js";
+import { SubscriptionThanks } from "./scenes/SubscriptionThanks/SubscriptionThanks.js";
+import { SubscriptionAlready } from "./scenes/SubscriptionAlready/SubscriptionAlready.js";
 
+// export const App = html`
+// 	<div class="page-wrap">
+// 		${AppBar}
+// 		<div class="main-content-wrap">${ArticlesBlock}</div>
+// 		${SubscriptionBlock}
+// 	</div>
+// `;
+
+//use when the design of the corresponding pages is ready
 export const App = html`
 	<div class="page-wrap">
-		${AppBar}
-		<div class="main-content-wrap">${ArticlesBlock}</div>
-		${SubscriptionBlock}
+		${router(
+			{
+				pattern: "/",
+				template: html`
+					${AppBar}
+					<div class="main-content-wrap">${ArticlesBlock}</div>
+					${SubscriptionBlock}
+				`,
+			},
+			{ pattern: "/thanks", template: SubscriptionThanks },
+			{ pattern: "/already-subscribed", template: SubscriptionAlready },
+			{
+				pattern: "/something-wrong",
+				template: html`<p>
+					Извините, что-то пошло не так... Попробуйте ещё раз позже
+				</p>`,
+			},
+			{ pattern: "", template: html`<p>404 Not Found</p>` }
+		)}
 	</div>
 `;
