1578daeb9fb85ac5be2b2a0b525d982003413e4c
Igor Klyopov <shoot8202@gmail.com>
2021-12-15 17:44:12 +0200

    adds draft of the handler

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,28 +1,29 @@
+const https = require("https");
+const BASIC_URL = "https://api.unisender.com/ru/api";
+const API_KEY = process.env.UNISENDER_API_KEY;
+const LIST_ID = process.env.LIST_ID;
+
 exports.handler = async function (event, context) {
+	const responce = await new Promise((resolve, reject) =>
+		https
+			.get(
+				`${BASIC_URL}/async/getContact?format=json&api_key=${API_KEY}&email=ivani@mail.com`,
+				(res) => {
+					res.setEncoding("utf8");
+					let rawData = "";
+					res.on("data", (chunk) => {
+						rawData += chunk;
+					});
+					res.on("end", () => {
+						resolve(rawData);
+					});
+				}
+			)
+			.on("error", reject)
+	);
+
 	return {
 		statusCode: 200,
-		body: JSON.stringify({ message: "Hello World" }),
+		body: responce,
 	};
 };
-
-///////////////////////////
-
-// `${BASIC_URL}/async/getContact?format=json&api_key=${API_KEY}&email=${email}`
-
-// const fetch = require("node-fetch");
-
-// const API_ENDPOINT = "https://cat-fact.herokuapp.com/facts";
-
-// exports.handler = async (event, context) => {
-// 	try {
-// 		const response = await fetch(API_ENDPOINT);
-// 		const data = await response.json();
-// 		return { statusCode: 200, body: JSON.stringify({ data }) };
-// 	} catch (error) {
-// 		console.log(error);
-// 		return {
-// 			statusCode: 500,
-// 			body: JSON.stringify({ error: "Failed fetching data" }),
-// 		};
-// 	}
-// };
