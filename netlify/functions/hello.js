f090c62275847f81daf3bba37920b014260c371c
Igor Klyopov <shoot8202@gmail.com>
2021-12-15 16:50:58 +0200

    adds draft

---
+++ netlify/functions/hello.js
@@ -0,0 +1,28 @@
+export const handler = async function (event, context) {
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: "Hello World" }),
+	};
+};
+
+///////////////////////////
+
+// `${BASIC_URL}/async/getContact?format=json&api_key=${API_KEY}&email=${email}`
+
+// const fetch = require("node-fetch");
+
+// const API_ENDPOINT = "https://cat-fact.herokuapp.com/facts";
+
+// exports.handler = async (event, context) => {
+// 	try {
+// 		const response = await fetch(API_ENDPOINT);
+// 		const data = await response.json();
+// 		return { statusCode: 200, body: JSON.stringify({ data }) };
+// 	} catch (error) {
+// 		console.log(error);
+// 		return {
+// 			statusCode: 500,
+// 			body: JSON.stringify({ error: "Failed fetching data" }),
+// 		};
+// 	}
+// };
