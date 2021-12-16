182717ac01ad851fcb66182b779179cffe704f70
Igor Klyopov <shoot8202@gmail.com>
2021-12-16 16:51:17 +0200

    remove unused code

--- src/services/unisenderAPI.js
+++ src/services/unisenderAPI.js
@@ -1,6 +1,4 @@
 const BASE_NETLIFY_URL = "/.netlify/functions";
-const API_KEY = process.env.UNISENDER_API_KEY;
-const LIST_ID = process.env.LIST_ID;
 
 function addContact(email) {
 	return fetch(`${BASE_NETLIFY_URL}/add-contact?email=${email}`).then((res) =>
