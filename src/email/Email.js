0917664cf5dc520ada47714e40d02aed906eaa95
Igor <shoot8202@gmail.com>
2022-01-04 12:20:47 +0200

    adds the ability to change the header text in the letter using jason from the date folder

--- src/email/Email.js
+++ src/email/Email.js
@@ -6,10 +6,10 @@ import { Section } from "../shared/index.js";
 import { Greeting } from "./components/Greeting/Greeting.js";
 import { ContentFooter } from "./components/ContentFooter/ContentFooter.js";
 
-export const Email = ({ sections, number }) =>
+export const Email = ({greetingTitle, greetingSubtitle, sections, number }) =>
 	html`
 		<div class="frame">
-			${Header({ issueNumber: number })}${Greeting}
+			${Header({ issueNumber: number })}${Greeting(greetingTitle,greetingSubtitle)}
 			<div class="content">${sections.map(Section)} ${ContentFooter}</div>
 			${Footer}
 		</div>
