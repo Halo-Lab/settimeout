017664f912ffbd19ab36fde598b8db6e53883026
Igor <shoot8202@gmail.com>
2022-01-04 15:01:31 +0200

    adds basic pagination logic

--- src/landing/components/Pagination/Pagination.js
+++ src/landing/components/Pagination/Pagination.js
@@ -1,29 +1,31 @@
-import { html, data } from "@prostory/edelweiss";
+import { html, data, effect } from "@prostory/edelweiss";
 
 import "./Pagination.css";
-// const getMailingNumber = data(1);
+
+export const getMailingNumber = data(0);
 
 
-export const Pagination = (getMailingNumber,lastMailingNumber) => {
-	getMailingNumber(lastMailingNumber);
+const onPreviousMailingBtnClick = () => {
 
-const onPaginationLinkClick = (e) => {
-	e.preventDefault();
-	
-	getMailingNumber(lastMailingNumber -= 1);
-	console.log("onPaginationLinkClick");
-	console.log('lastMailingNumber',lastMailingNumber);
-	
-	
+	let currentMailingNumber = getMailingNumber();
+
+	if (currentMailingNumber > 0) getMailingNumber(currentMailingNumber -= 1);
 };
-	
-	return html`
+
+const onNextMailingBtnClick = () => {
+
+	let currentMailingNumber = getMailingNumber();
+
+	getMailingNumber(currentMailingNumber += 1);
+};
+
+export const Pagination = html`
 	<div class="pagination">
 		<div class="container">
-			<a
+			<button
 				href="#"
-				class="pagination-link"
-				@click=${(e) => onPaginationLinkClick(e)}
+				class="pagination-button"
+				@click=${() => onPreviousMailingBtnClick()}
 			>
 				<svg
 					width="18"
@@ -38,7 +40,25 @@ const onPaginationLinkClick = (e) => {
 					/>
 				</svg>
 				<span>Предыдущая рассылка</span>
-			</a>
+			</button>
+			<button
+				href="#"
+				class="pagination-button"
+				@click=${() => onNextMailingBtnClick()}
+			>
+				<span>Следующая рассылка</span>
+				<svg width="18" 
+				height="14" 
+				viewBox="0 0 18 15" 
+				fill="currentColor" 
+				xmlns="http://www.w3.org/2000/svg"
+				class="arrow-right-icon"
+				>
+					<path 
+						d="M1 8.5C0.447715 8.5 0 8.05228 0 7.5C0 6.94772 0.447715 6.5 1 6.5L1 8.5ZM17.7071 6.79289C18.0976 7.18342 18.0976 7.81658 17.7071 8.20711L11.3431 14.5711C10.9526 14.9616 10.3195 14.9616 9.92893 14.5711C9.53841 14.1805 9.53841 13.5474 9.92893 13.1569L15.5858 7.5L9.92893 1.84315C9.53841 1.45262 9.53841 0.819457 9.92893 0.428932C10.3195 0.0384078 10.9526 0.0384078 11.3431 0.428932L17.7071 6.79289ZM1 6.5L17 6.5V8.5L1 8.5L1 6.5Z" fill="currentColor"
+					/>
+					</svg>
+			</button>
 		</div>
 	</div>
-`};
+`;
--- src/landing/scenes/Main/ArticlesBlock/ArticlesBlock.js
+++ src/landing/scenes/Main/ArticlesBlock/ArticlesBlock.js
@@ -1,41 +1,25 @@
-import { html, lazy, data, effect } from "@prostory/edelweiss";
+import { html, lazy, effect } from "@prostory/edelweiss";
 
 import { Section } from "~src/shared";
 import { IssueInfo } from "../../../components/IssueInfo/IssueInfo";
 import {
 	Pagination,
+	getMailingNumber
 } from "../../../components/Pagination/Pagination";
 
 import "./ArticlesBlock.css";
 
-const getMailingNumber = data();
 
 const emails = lazy(
 	() => fetch("/data.json").then((response) => response.json()),
 	[]
 );
 
+effect(()=>{getMailingNumber(emails().length - 1)})
 
 export const ArticlesBlock = () => {
-	const lastMailingNumber =
-		emails()?.length > 0 ? emails().length - 1 : emails().length;// лог начальное значение 0
-
-	// const lastMailingNumber = emails().length - 1;// лог начальное значение -1
-	console.log('lastMailingNumber', lastMailingNumber);
-
 	
-
-	const mailingNumber = getMailingNumber();
-	console.log('mailingNumber', mailingNumber);
-	
-	let articlesData = emails()[mailingNumber];
-	
-	
-
-	// const articlesData = emails()[emails().length - 1];
-	
-	
-	// console.log("articlesData", articlesData);
+	let articlesData = emails()[getMailingNumber()];
 
 	return html`
 		<section class="articles">
@@ -46,7 +30,7 @@ export const ArticlesBlock = () => {
 			<div class="container">
 				${articlesData ? articlesData.sections.map(Section) : ""}
 			</div>
-			${Pagination(getMailingNumber,lastMailingNumber)}
+			${Pagination}
 		</section>
 	`;
 };
