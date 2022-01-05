76c11bb7902a25e3ae1a4f252804e82f6d669e17
Igor <shoot8202@gmail.com>
2022-01-05 16:47:53 +0200

    makes it possible to add any text as a tag - if its properties are not encoded in /shared/index.js - text with default styles for an unknown (unencoded tag) will be displayed

--- src/shared/Card/Card.js
+++ src/shared/Card/Card.js
@@ -9,7 +9,7 @@ const buildTags = (tags) =>
 		return html`
 			<span class="item-tag item-tag--${key}">
 				${url ? html` <img class="item-tag-ico" .src="${url}" alt="${tagName} icon" width="14" height="10"/> ` : ""}
-				#${tagName}
+				#${tagName ?? name}
 			</span>
 		`;
 	});
