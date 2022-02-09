b61188c8602a19bf54e639d2f479f22eeb3b3f1b
MityaDementiy <d.dementiy@gmail.com>
2022-02-09 17:45:57 +0200

    Add error page basic layout

--- src/landing/scenes/Error/Error.js
+++ src/landing/scenes/Error/Error.js
@@ -1,8 +1,12 @@
-import { html } from "@prostory/edelweiss";
+import { html, current } from "@prostory/edelweiss";
 
 import { getImageURL, Image } from "../../../shared/index.js";
 
 import "./Error.css";
+
+const onBackHomeLinkClick = () => {
+	current("/");
+};
 
 export const ErrorPage = html`
 	<div class="error-page">
@@ -15,14 +19,28 @@ export const ErrorPage = html`
 				  width="241"
 				  height="36"
 				/>
-				<a href="mailto:hey@settimeout.dev" class="settimeout-mailto-link"
+				<a href="mailto:hey@settimeout.dev" class="mailto-link"
 					>📨 hey@settimeout.dev</a
 				>
 			</div>
 		</header>
 		<main class="main-content">
           <div class="main-content-container"> 
-            <img src=${getImageURL(Image.NOPAGEIMAGE)} />
+            <img 
+			  src=${getImageURL(Image.NOPAGEIMAGE)} 
+			  loading="eager" 
+			  class="notfound-image"
+			  width="588"
+			  height="327"
+			/>
+			<h1 class="main-headline">Страница не найдена :(</h1>
+			<p class="notfound-message">Вернитесь на главную, чтоб увидеть много полезной информации</p>
+			<button
+			  class="backhome-button"
+			  @click=${(e) => onBackHomeLinkClick(e)}
+			>
+			  Вернуться на главную
+			</button>
 		  </div>
 		</main>
 	</div>
